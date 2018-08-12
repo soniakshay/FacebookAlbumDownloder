0fed3f60ea8163c5d4e62070e4d1a8b3ecae131c
Akshay <soniiakshayy8000@gmail.com>
2018-08-12 10:59:41 +0530

    list the album with  name and cover photo
    list the album with cover photo and album name  and create design responsive also (using bootstrap)

--- page/home.php
+++ page/home.php
@@ -36,6 +36,16 @@ if(isset($access_token)) {
 
 
 
+$url="https://graph.facebook.com/v3.1/me?fields=albums&access_token=".$access_token;
+$result = file_get_contents($url);
+$album=json_decode($result);
+
+$url1="https://graph.facebook.com/v3.1/me?fields=albums%7Bpicture%7D&access_token=".$access_token;
+$result = file_get_contents($url1);
+$pic=json_decode($result);
+
+
+
 ?>
 <html>
     <head>
@@ -65,6 +75,45 @@ if(isset($access_token)) {
             
         </div>  
     </nav>
-       
+       <div class="container">
+            <div class="row">
+                <?php
+                
+                    foreach($album->albums->data as $mydata)
+                    {
+                        
+                        foreach($pic->albums->data as $mydata1)
+                        {
+                            if($mydata1->id==$mydata->id)
+                            {
+                ?>
+                                <div class="col-lg-3 col-md-3  col-sm-12 col-xm-12" style="height:400px;padding:2px;">
+                                    <div id="box" style="border:1px solid;">
+                                        <div id="image" style="height:80%;width:100%;background:url('<?php echo $mydata1->picture->data->url;?>');background-position:center;background-size:cover;    ">
+                                        </div>
+                                        <h4 style="margin-left:10px;"><?php echo $mydata->name; ?></h4>
+                                        <div id="buttongrp">
+                                            <center>
+                                             <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-eye-open "></span><span class="btntext" style="margin-left:5px;">View</span></button>
+                                            <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-save"></span><span class="btntext" style="margin-left:5px;">downloud</span></button>  
+                                            <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-share"></span><span class="btntext" style="margin-left:5px;">Share</span></button>
+
+                                        </div>    
+                                    </div>
+                                </div>
+                            
+                <?php
+                                break;
+                            }
+                        }
+                        
+                    }
+                ?>
+            
+           
+           <!--https://graph.facebook.com/me/albums?limit=25&before=
+-->           
+        </div>
+        </div>
     </body>
 </html>
